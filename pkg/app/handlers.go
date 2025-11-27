a46bc35f63539323d772c56d87b9783cc8895ee0
Bastian Rihm <me@bastianjoel.de>
2025-11-27 22:07:52 +0100

    Fix asset serving

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -1,25 +1,57 @@
 package app
 
 import (
+	"errors"
+	"io/fs"
+	"mime"
 	"net/http"
-	"os"
+	"path"
+	"strings"
 
 	"github.com/invopop/ctxi18n/i18n"
 	"github.com/labstack/echo/v4"
 )
 
 func (a *App) serveClientAppHandler(c echo.Context) error {
-	requestPath := c.Request().URL.Path
+	if a.Assets == nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "assets filesystem not configured")
+	}
+
+	requestPath := strings.TrimPrefix(c.Request().URL.Path, a.WebRoot())
 	if requestPath == "" || requestPath == "/" {
-		return c.File("assets/client/index.html")
+		return a.serveClientAsset(c, "client/index.html")
 	}
-	
-	filePath := "assets/client" + requestPath
-	if _, err := os.Stat(filePath); err == nil {
-		return c.File(filePath)
+
+	normalized := path.Clean(requestPath)
+	normalized = strings.TrimPrefix(normalized, "/")
+	if normalized == "" || normalized == "." {
+		return a.serveClientAsset(c, "client/index.html")
 	}
-	
-	return c.File("assets/client/index.html")
+
+	assetPath := path.Join("client", normalized)
+	if _, err := fs.Stat(a.Assets, assetPath); err == nil {
+		return a.serveClientAsset(c, assetPath)
+	}
+
+	return a.serveClientAsset(c, "client/index.html")
+}
+
+func (a *App) serveClientAsset(c echo.Context, assetPath string) error {
+	data, err := fs.ReadFile(a.Assets, assetPath)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return echo.NewHTTPError(http.StatusNotFound)
+		}
+
+		return err
+	}
+
+	contentType := mime.TypeByExtension(path.Ext(assetPath))
+	if contentType == "" {
+		contentType = http.DetectContentType(data)
+	}
+
+	return c.Blob(http.StatusOK, contentType, data)
 }
 
 func (a *App) redirectWithError(c echo.Context, target string, err error) error {
