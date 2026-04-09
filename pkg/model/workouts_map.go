c1974fb3b2d5e2457d25e6650e4731542b53f46d
Bastian Rihm <brihm@intevation.de>
2026-04-09 15:09:05 +0200

    Save extra metrics in jsonb column

--- pkg/model/workouts_map.go
+++ pkg/model/workouts_map.go
@@ -106,17 +106,17 @@ type MapPoint struct {
 
 	Time time.Time `json:"time"` // The time the point was recorded
 
-	ExtraMetrics    ExtraMetrics  `gorm:"serializer:json" json:"extraMetrics"` // Extra metrics at this point
-	Lat             float64       `json:"lat"`                                 // The latitude of the point
-	Lng             float64       `json:"lng"`                                 // The longitude of the point
-	Elevation       float64       `json:"elevation"`                           // The elevation of the point
-	Distance        float64       `json:"distance"`                            // The distance from the previous point
-	Distance2D      float64       `json:"distance2D"`                          // The 2D distance from the previous point
-	TotalDistance   float64       `json:"totalDistance"`                       // The total distance of the workout up to this point
-	TotalDistance2D float64       `json:"totalDistance2D"`                     // The total 2D distance of the workout up to this point
-	Duration        time.Duration `json:"duration"`                            // The duration from the previous point
-	TotalDuration   time.Duration `json:"totalDuration"`                       // The total duration of the workout up to this point
-	SlopeGrade      float64       `json:"slopeGrade"`                          // The grade of the slope at this point
+	ExtraMetrics    ExtraMetrics  `json:"extraMetrics"`    // Extra metrics at this point
+	Lat             float64       `json:"lat"`             // The latitude of the point
+	Lng             float64       `json:"lng"`             // The longitude of the point
+	Elevation       float64       `json:"elevation"`       // The elevation of the point
+	Distance        float64       `json:"distance"`        // The distance from the previous point
+	Distance2D      float64       `json:"distance2D"`      // The 2D distance from the previous point
+	TotalDistance   float64       `json:"totalDistance"`   // The total distance of the workout up to this point
+	TotalDistance2D float64       `json:"totalDistance2D"` // The total 2D distance of the workout up to this point
+	Duration        time.Duration `json:"duration"`        // The duration from the previous point
+	TotalDuration   time.Duration `json:"totalDuration"`   // The total duration of the workout up to this point
+	SlopeGrade      float64       `json:"slopeGrade"`      // The grade of the slope at this point
 }
 
 func (MapPoint) TableName() string {
